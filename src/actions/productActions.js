01f1f98241d48f359f8df0ec5a4b72756969b6f8
Braveheart ola <ola20sarumi@hotmail>
2020-08-07 12:00:59 +0300

    update of the state management(redux) e with '#' will be ignored, and an empty message aborts the commit.

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 import axios from "axios";
 
@@ -15,16 +18,14 @@ const listProducts = () => async (dispatch) => {
   }
 };
 
-const detailsProduct = (productId) => (dispatch) => {
+const detailsProduct = (productId) => async (dispatch) => {
   try {
-    dispatch({type: PRODUCT_DETAILS_REQUEST,payload: productId})
-    const {data} = await axios.get("/api/products" + productId);
-    dispatch ({type: PRODUCT_DETAILS_SUCCESS, payload: data})
-  } catch (error) { 
-    dispatch({PRODUCT_DETAILS_FAIL, payload: error.message })
-    
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+    const { data } = await axios.get("/api/products/" + productId);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ PRODUCT_DETAILS_FAIL, payload: error.message });
   }
+};
 
-}
-
-export { listProducts };
+export { listProducts, detailsProduct };
